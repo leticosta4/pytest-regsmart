7295369e2276f56887b5ce4858b36a94aa943839
let <leticiacostaoa@gmail.com>
2026-06-26 00:18:49 -0300

    refactor: separate help strings

--- src/pytest_ranking/plugin.py
+++ src/pytest_ranking/plugin.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import argparse
 import os
 import random
-import textwrap
 import time
 from enum import Enum
 
@@ -18,46 +17,9 @@ from _pytest.terminal import TerminalReporter
 
 from .const import (DATA_DIR, DEFAULT_HIST_LEN, DEFAULT_LEVEL, DEFAULT_REPLAY,
                     DEFAULT_SEED, DEFAULT_WEIGHT, LEVEL)
+from .help_strings import (HIST_LEN_HELP, LEVEL_HELP, PLUGIN_HELP,
+                           REPLAY_HELP, SEED_HELP, WEIGHT_HELP)
 from .rank import get_ranking
-
-PLUGIN_HELP = textwrap.dedent("""\
-Run regression test prioritization for pytest test suite.
-It re-orders execution of tests to expose test failure sooner.
-""")
-
-
-WEIGHT_HELP = textwrap.dedent("""\
-Set weights on different prioritization heuristics,
-separated by hyphens `-`.
-The sum of weights will be normalized to 1.
-Higher weight means that heuristic will be favored.
-Default value is 1-0.
-""")
-
-HIST_LEN_HELP = textwrap.dedent("""\
-The maximum number of previous test runs
-that can be recorded for a test since the test has failed.
-Default value is 50 (must be integer).
-""")
-
-SEED_HELP = textwrap.dedent("""\
-Seed when running tests in random order.
-You can run random order via setting `--rank-weight=0-0-0`
-Default value is 0.
-""")
-
-LEVEL_HELP = textwrap.dedent("""
-The test group level at which the prioritization takes place.
-Test items below the configured level follow pytest default order.
-Score of a test group is the mean score over all tests in that group.
-Default value is PUT.
-""")
-
-REPLAY_HELP = textwrap.dedent("""
-Provide a text file where each line is a test ID.
-pytest-ranking will run tests with the order defined in the file.  
-Default value is None.
-""")  ## ver ainda o que fazer sobre isso
 
 
 def pytest_addoption(parser: Parser) -> None:
